eca370e605815695c1c66da12f825432f4d11ae1
Enow Mbi <benowmbi@yahoo.com>
2021-11-26 13:19:19 +0100

    Add todos#create action

--- app/controllers/api/v1/todos_controller.rb
+++ app/controllers/api/v1/todos_controller.rb
@@ -13,6 +13,17 @@ module Api
         render json: todos
       end
 
+      def create
+        todo = Todo.new(todo_params)
+        if todo.save!
+          render(json: todo, status: :created)
+        else
+          render(json: todo.errors.full_messages, status: :unprocessable_entity)
+        end
+      end
+
+      private
+
       def limit
         params[:limit] ? params[:limit].to_i : 25
       end
@@ -22,6 +33,10 @@ module Api
         offset_value = limit * page_number
         number_of_todos = Todo.all.size
         offset_value > number_of_todos ? number_of_todos : offset_value
+      end
+
+      def todo_params
+        params.require(:todo).permit(:title, :description, :complete)
       end
     end
   end
