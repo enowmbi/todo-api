8852d009cb427c95058dc4b02e0ca3fda459af4d
Enow Mbi <benowmbi@yahoo.com>
2021-11-26 11:34:45 +0100

    Create todos model and specs

---
+++ db/migrate/20211126102938_create_todos.rb
@@ -0,0 +1,11 @@
+class CreateTodos < ActiveRecord::Migration[6.1]
+  def change
+    create_table :todos do |t|
+      t.string :title
+      t.text :description
+      t.boolean :complete
+
+      t.timestamps
+    end
+  end
+end
---
+++ spec/factories/todos.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :todo do
+    title { "MyString" }
+    description { "MyText" }
+    complete { false }
+  end
+end
