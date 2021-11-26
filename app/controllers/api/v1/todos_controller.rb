868eef83a9de2fab855ff1a8a6d73ed8bad2e574
Enow Mbi <benowmbi@yahoo.com>
2021-11-26 12:53:32 +0100

    Fix index action to allow limits and offsets

--- app/controllers/api/v1/todos_controller.rb
+++ app/controllers/api/v1/todos_controller.rb
@@ -8,17 +8,20 @@ module Api
         todos = Todo
                 .all
                 .limit(limit)
-                .offset(page_number)
+                .offset(offset)
 
         render json: todos
       end
 
       def limit
-        params[:limit] || 25
+        params[:limit] ? params[:limit].to_i : 25
       end
 
-      def page_number
-        params[:page_number] || 0
+      def offset
+        page_number = params[:page_number].to_i < 1 ? 0 : params[:page_number].to_i
+        offset_value = limit * page_number
+        number_of_todos = Todo.all.size
+        offset_value > number_of_todos ? number_of_todos : offset_value
       end
     end
   end
