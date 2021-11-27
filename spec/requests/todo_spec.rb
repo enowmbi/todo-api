b15c56cf0dae4c7edb443ed5bf7a8dd297aa40b9
Enow Mbi <benowmbi@yahoo.com>
2021-11-27 22:56:02 +0100

    Add specs for todos#create -- with valid_params

--- spec/requests/todo_spec.rb
+++ spec/requests/todo_spec.rb
@@ -22,4 +22,53 @@ RSpec.describe "Todos", type: :request do
       expect([JSON.parse(response.body)].size).to eq(1)
     end
   end
+
+  describe "POST /api/v1/todos" do
+    let(:valid_params) do
+      {
+        todo:
+        {
+          title: "A new todo title",
+          description: "New todo description",
+          complete: "true"
+        }
+      }
+    end
+
+    let(:invalid_params) do
+      {
+        todo:
+        {
+          title: "A new todo title",
+          description: "New todo description",
+          complete: "true"
+        }
+      }
+    end
+
+    describe "with valid params" do
+
+      before(:each){ post "/api/v1/todos", params: valid_params }
+
+      it "returns http created" do
+        expect(response).to have_http_status(:created)
+      end
+
+      it "increments todo collection by one(1)" do
+        expect{post "/api/v1/todos", params: valid_params}.to change(Todo, :count).by(1)
+      end
+
+      it "returns created todo as json" do
+        expect(response.content_type).to eq("application/json; charset=utf-8")
+      end
+    end
+
+    xdescribe "with invalid params" do
+      before(:each){ post "/api/v1/todos", params: invalid_params }
+
+      it "returns http unprocessable_entity" do
+        expect(response).to have_http_status(:unprocessable_entity)
+      end
+    end
+  end
 end
