61d2235f2441fb8ef2305cebde905cb1dc0205d3
Enow Mbi <benowmbi@yahoo.com>
2021-11-27 22:30:15 +0100

    Add specs for todos#show action

--- spec/requests/todo_spec.rb
+++ spec/requests/todo_spec.rb
@@ -4,9 +4,22 @@ RSpec.describe "Todos", type: :request do
   describe "GET /api/v1/todos/index" do
     it "returns http success" do
       FactoryBot.create(:todo, complete: true)
+      FactoryBot.create(:todo, title: "another todo title", description: "another todo description", complete: true)
 
       get "/api/v1/todos"
       expect(response).to have_http_status(:success)
+      expect(JSON.parse(response.body).size).to eq(2)
+    end
+  end
+
+  describe "GET /api/v1/todos/id" do
+    it "returns http success" do
+      todo = FactoryBot.create(:todo, complete: true)
+      FactoryBot.create(:todo, title: "another todo title", description: "another todo description", complete: true)
+
+      get "/api/v1/todos/#{todo.id}"
+      expect(response).to have_http_status(:success)
+      expect([JSON.parse(response.body)].size).to eq(1)
     end
   end
 end
