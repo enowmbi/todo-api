45205121edcfd1237dc2cd8db1b93fb462f0a9e6
Enow Mbi <benowmbi@yahoo.com>
2021-11-27 22:08:01 +0100

    Add specs for todos#index method

--- spec/requests/todo_spec.rb
+++ spec/requests/todo_spec.rb
@@ -1,11 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe "Todos", type: :request do
-  describe "GET /index" do
+  describe "GET /api/v1/todos/index" do
     it "returns http success" do
-      get "/todo/index"
+      FactoryBot.create(:todo, complete: true)
+
+      get "/api/v1/todos"
       expect(response).to have_http_status(:success)
     end
   end
-
 end
